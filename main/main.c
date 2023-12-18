403cf9c776e6caff4f4f25db087136b578136457
wallacegibbon <wallacegibbon@aliyun.com>
2023-12-19 01:31:25 +0800

    Upgrade: Code got updated to adapt to the new `screen-library-mcu` library.

--- main/main.c
+++ main/main.c
@@ -1,24 +1,24 @@
-#include "sc_ssd1306_esp32_i2c.h"
-#include "sc_ssd1306.h"
-#include "sc_st7735_esp32_softspi.h"
-#include "sc_st7735.h"
-#include "sc_painter.h"
-#include "sc_common.h"
-#include "sc_color.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "sc_color.h"
+#include "sc_common.h"
+#include "sc_painter.h"
+#include "sc_ssd1306.h"
+#include "sc_ssd1306_esp32_i2c.h"
+#include "sc_st7735.h"
+#include "sc_st7735_esp32_softspi.h"
 
-void fancy_display_1(struct Painter *painter) {
+void fancy_display_1(struct painter *painter) {
 	static int current_cnt = 0, step = 1;
-	struct Point p;
+	struct point p;
 	int color, i;
 
-	Point_initialize(&p, 64, 32);
+	point_initialize(&p, 64, 32);
 	for (i = 0; i < 31; i++) {
 		color = current_cnt == i ? BLACK_16bit : GREEN_16bit;
-		Painter_draw_circle(painter, p, i, color);
+		painter_draw_circle(painter, p, i, color);
 	}
-	Painter_flush(painter);
+	painter_flush(painter);
 
 	if (current_cnt == 31)
 		step = -1;
@@ -28,89 +28,80 @@ void fancy_display_1(struct Painter *painter) {
 	current_cnt += step;
 }
 
-void initialize_screen_1(
-	struct SSD1306_Screen *screen1,
-	struct SSD1306_ScreenAdaptorESP32I2C *adaptor1
-) {
+void initialize_screen_1(struct ssd1306_screen *screen, struct ssd1306_adaptor_esp32_i2c *adaptor) {
 	printf("initializing SSD1306...\r\n");
 
-	SSD1306_ScreenAdaptorESP32I2C_initialize(
-		adaptor1, 0x3C, I2C_NUM_0
-	);
+	ssd1306_adaptor_esp32_i2c_initialize(adaptor, 0x3C, I2C_NUM_0);
 
-	SSD1306_Screen_initialize(
-		screen1,
-		(struct SSD1306_ScreenAdaptorInterface **) adaptor1
-	);
+	ssd1306_initialize(
+		screen,
+		(const struct ssd1306_adaptor_i **)adaptor);
 
 	printf("SSD1306 screen on...\r\n");
-	SSD1306_Screen_display_on(screen1);
+	ssd1306_display_on(screen);
 
-	//printf("setting SSD1306 screen to 32-row mode...\r\n");
-	//SSD1306_Screen_fix_32row(&screen);
+	// printf("setting SSD1306 screen to 32-row mode...\r\n");
+	// SSD1306_Screen_fix_32row(&screen);
 }
 
 void initialize_screen_2(
-	struct ST7735_Screen *screen2,
-	struct ST7735_ScreenAdaptorESP32SoftSPI *adaptor2
-) {
+	struct st7735_screen *screen2,
+	struct st7735_adaptor_esp32_soft_spi *adaptor2) {
+
 	/// start the BG LED of T-Dongle-S3
 	ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_38, GPIO_MODE_OUTPUT));
 	ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_38, 0));
 
 	printf("initializing ST7735...\r\n");
 
-	ST7735_ScreenAdaptorESP32SoftSPI_initialize(
-		adaptor2, GPIO_NUM_3, GPIO_NUM_5, GPIO_NUM_4, GPIO_NUM_1, GPIO_NUM_2
-	);
+	st7735_adaptor_esp32_soft_spi_initialize(
+		adaptor2, GPIO_NUM_3, GPIO_NUM_5, GPIO_NUM_4, GPIO_NUM_1, GPIO_NUM_2);
 
-	ST7735_Screen_initialize(
+	st7735_initialize(
 		screen2,
-		(struct ST7735_ScreenAdaptorInterface **) adaptor2
-	);
+		(const struct st7735_adaptor_i **)adaptor2);
 }
 
 void app_main() {
-	struct SSD1306_ScreenAdaptorESP32I2C adaptor1;
-	struct SSD1306_Screen screen1;
-	struct ST7735_ScreenAdaptorESP32SoftSPI adaptor2;
-	struct ST7735_Screen screen2;
-	struct Painter painter;
-	struct Point p1;
-	struct Point p2;
+	struct ssd1306_adaptor_esp32_i2c adaptor1;
+	struct ssd1306_screen screen1;
+	struct st7735_adaptor_esp32_soft_spi adaptor2;
+	struct st7735_screen screen2;
+	struct painter painter;
+	struct point p1;
+	struct point p2;
 
-	//initialize_screen_1(&screen1, &adaptor1);
+	// initialize_screen_1(&screen1, &adaptor1);
 	initialize_screen_2(&screen2, &adaptor2);
 
-	//SSD1306_Screen_set_up_down_invert(&screen1);
+	// ssd1306_screen_set_up_down_invert(&screen1);
 
-	//painter.drawing_board = (struct DrawingBoardInterface **) &screen1;
-	painter.drawing_board = (struct DrawingBoardInterface **) &screen2;
+	// painter.drawing_board = (struct drawing_i **) &screen1;
+	painter.drawing_board = (const struct drawing_i **)&screen2;
 
 	printf("clearing screen...\r\n");
-	Painter_clear(&painter, BLACK_16bit);
+	painter_clear(&painter, BLACK_16bit);
 
 	printf("drawing a rectangle...\r\n");
-	Point_initialize(&p1, 64 - 50, 32 - 20);
-	Point_initialize(&p2, 64 + 50, 32 + 20);
-	Painter_draw_rectangle(&painter, p1, p2, BLUE_16bit);
+	point_initialize(&p1, 64 - 50, 32 - 20);
+	point_initialize(&p2, 64 + 50, 32 + 20);
+	painter_draw_rectangle(&painter, p1, p2, BLUE_16bit);
 
 	printf("drawing a circle on top left...\r\n");
-	Point_initialize(&p1, 64 - 50, 32 - 20);
-	Painter_draw_circle(&painter, p1, 5, RED_16bit);
+	point_initialize(&p1, 64 - 50, 32 - 20);
+	painter_draw_circle(&painter, p1, 5, RED_16bit);
 
 	/*
 	printf("drawing a line...\r\n");
-	Point_initialize(&p1, 30, 10);
-	Point_initialize(&p2, 20, 50);
-	Painter_draw_line(&painter, p1, p2, WHITE_1bit);
+	point_initialize(&p1, 30, 10);
+	point_initialize(&p2, 20, 50);
+	painter_draw_line(&painter, p1, p2, WHITE_1bit);
 	*/
 
 	printf("flushing the screen...\r\n");
-	Painter_flush(&painter);
+	painter_flush(&painter);
 
 	while (1) {
 		fancy_display_1(&painter);
 	}
 }
-
